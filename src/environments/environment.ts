af2871d10d60254043461d47ea1d5677f3ee85b8
Jos Vermoesen <info@rv.be>
2020-03-27 05:28:43 +0100

    RR number checking

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,10 +12,12 @@ export const environment = {
 
   // https should be http when SSL not enabled
   // marIntegraal Service Plus users can get our dotnet core api source code
-  apiUrl: 'http://localhost:5000/api/',
+  /* apiUrl: 'http://localhost:5000/api/',
   apiWhiteListDomain: ['localhost:5000'],
-  apiBlackListDomain: ['localhost:5000/api/auth'],
-
+  apiBlackListDomain: ['localhost:5000/api/auth'], */
+  apiUrl: 'https://rv-services.be/api/',
+  apiWhiteListDomain: ['rv-services.be'],
+  apiBlackListDomain: ['rv-services.be/api/auth'],
   // vsoft!
   contentful: {
     spaceId: 'mq8ieqd7mcv8',
